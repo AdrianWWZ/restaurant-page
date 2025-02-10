8dc043387a8cfef17cf856f0858edd7bd62e61bf
Adrian Wong <adrianwongapple@gmail.com>
2025-02-10 15:52:24 +0800

    Finished the menu page in html

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import "./style.css";
 import { main } from "./homePage.js";
 
+let currPage = "homePage";
 const currentContent = document.querySelector("#content");
-currentContent.appendChild(main);
+//currentContent.appendChild(main);
