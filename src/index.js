73cef13eeed01ebb1851b25db6fda85d84c680ef
Adrian Wong <adrianwongapple@gmail.com>
2025-02-11 16:07:03 +0800

    Did the js logic in index.js to switch between pages

--- src/index.js
+++ src/index.js
@@ -4,4 +4,34 @@ import { menuPage } from "./menuPage.js";
 
 let currPage = "homePage";
 const currentContent = document.querySelector("#content");
-currentContent.appendChild(menuPage);
+
+const r = document.querySelector(":root");
+
+const removePage = () => {
+  while (currentContent.firstChild)
+    currentContent.removeChild(currentContent.firstChild);
+};
+
+document.querySelector(".home-button").addEventListener("click", () => {
+  if (currPage !== "homePage") {
+    currPage = "homePage";
+    removePage();
+    currentContent.appendChild(homePage);
+    r.style.setProperty("--main-color", "#f1f5f9");
+    r.style.setProperty("--sub-color", "#cad5e2");
+    r.style.setProperty("--page-height", "125%");
+  }
+});
+
+document.querySelector(".menu-button").addEventListener("click", () => {
+  if (currPage !== "menuPage") {
+    currPage = "menuPage";
+    removePage();
+    currentContent.appendChild(menuPage);
+    r.style.setProperty("--main-color", "#ffccd3");
+    r.style.setProperty("--sub-color", "#ffa2a2");
+    r.style.setProperty("--page-height", "140%");
+  }
+});
+
+currentContent.appendChild(homePage);
